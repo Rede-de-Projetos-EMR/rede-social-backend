05496f666cc566fbe6ff31bb11e53cde38666c32
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-05-23 10:44:03 -0300

    adiciona entities de user

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { randomUUID } from "crypto";
+import { Exclude } from "class-transformer";
+
+export class User {
+  readonly id: string;
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+  country: string;
+  bio:string;
+  avatarUrl: string;
+  userName: string;
+  email: string;
+  isActive: boolean;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+
+  @Exclude()
+  password: string;
+
+  constructor(){
+    this.id = randomUUID();
+  }
+}
