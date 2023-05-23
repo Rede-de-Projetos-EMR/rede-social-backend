99b9bce683bbbd50f9dbe72610170b0cc3c14c88
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-05-23 10:55:59 -0300

    adiciona dto de user

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,45 @@
-export class CreateUserDto {}
+import { hashSync } from "bcryptjs";
+import { Transform } from "class-transformer";
+import { IsDate, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  birthDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  country: string;
+
+  @IsString()
+  @IsNotEmpty()
+  bio: string;
+
+  @IsString()
+  @IsNotEmpty()
+  avatarUrl: string;
+
+  @IsString()
+  @IsNotEmpty()
+  userName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @MinLength(8)
+  @MaxLength(60)
+  @IsNotEmpty()
+  @Transform(({ value }: {value: string}) => hashSync(value, 10),{
+    groups: ["transform"]
+  })
+  password: string;
+}
