6a855ad66d7a10b41dce62b4bb1bc697a51dadac
Elton Luiz Scholze <elscholze@gmail.com>
2023-06-01 18:50:34 -0300

    fix: adicionado middleware FindPost no module de comment, corrigido classe de erro nos middlewares de comment

--- src/modules/comment/comment.module.ts
+++ src/modules/comment/comment.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
 import { CommentService } from "./comment.service";
 import { CommentController } from "./comment.controller";
 import { PrismaService } from "src/prisma/prisma.service";
@@ -6,17 +11,38 @@ import { CommentRepository } from "./repositories/comment.repository";
 import { CommentPrismaRepository } from "./repositories/prisma/prisma.repository";
 import { CommentOwner } from "src/middlewares/comment/commentOwner";
 import { CommentOwnerAndPostOwner } from "src/middlewares/comment/commentOwnerAndPostOwner";
+import { FindPost } from "src/middlewares/post/findPost";
+import { PostRepository } from "../post/repositories/post.repository";
+import { PostPrismaRepository } from "../post/repositories/prisma/prisma.repository";
 
 @Module({
   controllers: [CommentController],
-  providers: [CommentService, PrismaService, {
-    provide: CommentRepository,
-    useClass: CommentPrismaRepository
-  }]
+  providers: [
+    CommentService,
+    PrismaService,
+    {
+      provide: CommentRepository,
+      useClass: CommentPrismaRepository,
+    },
+    {
+      provide: PostRepository,
+      useClass: PostPrismaRepository,
+    },
+  ],
 })
-export class CommentModule implements NestModule{
-  configure(consumer: MiddlewareConsumer){
-    consumer.apply(CommentOwner).forRoutes({path: "comment/*", method: RequestMethod.PATCH});
-    consumer.apply(CommentOwnerAndPostOwner).forRoutes({path: "comment/*", method: RequestMethod.DELETE});
+export class CommentModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(FindPost)
+      .forRoutes(
+        { path: "comment/*", method: RequestMethod.POST },
+        { path: "comment/*", method: RequestMethod.GET },
+      );
+    consumer
+      .apply(CommentOwner)
+      .forRoutes({ path: "comment/*", method: RequestMethod.PATCH });
+    consumer
+      .apply(CommentOwnerAndPostOwner)
+      .forRoutes({ path: "comment/*", method: RequestMethod.DELETE });
   }
 }
